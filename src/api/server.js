935bb70dadf84496d23461f4a0925e8f38550b60
Ssuyu Ke <ssuyuke@asy.tw>
2024-12-28 15:15:58 +0800

    update

--- src/api/server.js
+++ src/api/server.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from '@/utils/auth';
 
 const server = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: '/'
 });
 
 server.interceptors.request.use(
